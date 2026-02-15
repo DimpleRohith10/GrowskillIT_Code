aa264ffec4ba8400017947b4a8fae3cebb782c22
dimple.reddy <dimple.reddy@pelatro.com>
2026-02-15 23:57:56 +0530

    New Assignment

---
+++ src/assignment2/Program1_Smallestof2numbers.java
@@ -0,0 +1,19 @@
+package assignment2;
+
+public class Program1_Smallestof2numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=2;
+		
+		if(a<b)
+		{
+			System.out.println("Smallest Number is : "+a);
+		}
+		else 
+		{
+			System.out.println("Smallest Number is : "+b);
+		}
+	}
+
+}
